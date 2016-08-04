a5770b16e53e2e5ea74d8ae73c1e698bf5ccb521
kaziida24 <kaziida24@gmail.com>
2016-08-03 23:22:34 -0500

    Wrote help menu for visuazliation program and added example ASCII file.

--- src/MATLAB/visualizeDataset.m
+++ src/MATLAB/visualizeDataset.m
@@ -1,31 +1,49 @@
-function visualizeDataset()
+function visualizeDataset(varargin)
 
 % Need MATLAB R2016a with the computer vision toolbox to use this
 % visualization program 
 
-% filename = '../datasets/Wrapped_Orange 1 (copy).txt'; 
-filename = '../../datasets/Wrapped_Orange 1 (copy).txt';
+if (nargin ~= 1)
+    fprintf('Check usage again.\n'); 
+    return; 
+else
+    if (strcmp(varargin{1,1}, '-h'))
+        help_menu(); 
+        return;
+    else
+        dataset_name = varargin{1,1}; 
+    end
+end
+
+filename = sprintf('../datasets/%s', dataset_name); 
 points = dlmread(filename); 
-dataset_name = parseString(filename); 
 
 close all force; 
 figure();
 pcshow(points); 
 grid on; 
-title(sprintf('Rendering of %s\n', dataset_name), 'Interpreter', 'none'); 
+title(sprintf('Rendering of %s', dataset_name), 'Interpreter', 'none'); 
 xlabel('x');
 ylabel('y');
 zlabel('z');
 
-    function output_string = parseString(input_string)
-       % Parses path string so that it only includes the textfile name
-       for i = length(input_string):-1:1
-           if (strcmp(input_string(i), '/') == 1)
-               % Divide up the string here
-               output_string = input_string(i+1:length(input_string)); 
-               break; 
-           end
-
-       end
+    function help_menu()
+        fprintf('\n'); 
+        fprintf('Welcome to the help menu for visualizeDataset.\n');
+        fprintf('\n'); 
+        fprintf('Usage:\n\n'); 
+        fprintf('Call the program with the name of the ASCII file to render.\n'); 
+        fprintf('visualizeDataset filename.txt\n\n'); 
+        fprintf('See the help menu again.\n'); 
+        fprintf('visualizeDataset -h\n\n');
+        fprintf('List of ASCII files in the datasets folder\n\n'); 
+        dir_info = dir('../datasets'); 
+        for i = 1:size(dir_info)
+            currentFile = dir_info(i).name; 
+            if (length(currentFile) > 4 && strcmp(currentFile(length(currentFile)-3:length(currentFile)), '.txt'))
+                fprintf('%s\n', currentFile); 
+            end
+        end
+        fprintf('\n'); 
     end
 end
